dc59216e270e2c220371d51076f35df202b76caa
Reza Abbasi <rz.abs.shi@gmail.com>
2018-06-28 00:36:46 +0430

    some bugs fixed

--- _base/urls.py
+++ _base/urls.py
@@ -1,4 +1,4 @@
-"""_base2 URL Configuration
+"""_base URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
